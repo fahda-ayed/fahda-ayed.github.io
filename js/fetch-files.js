73003fe93a2ed61f8c26a4278764070d542bf8d8
ahmadfathy97 <af1381@fayoum.edu.eg>
2022-06-23 16:53:33 +0200

    00

--- js/fetch-files.js
+++ js/fetch-files.js
@@ -3,10 +3,20 @@ let url = new URL(url_string);
 let id = url.searchParams.get("id");
 let type = url.searchParams.get("type");
 
-
-fetch("../data/files.json")
-.then(res => res.json())
-.then(data=>{
+if(id && type !="CV"){
+  fetch("../data/files.json")
+  .then(res => res.json())
+  .then(data=>{
     document.querySelector('#file-name').textContent = data[type][id];
     document.querySelector('iframe').setAttribute('src',`../files/${type}/${data[type][id]}.pdf`);
-});
+    document.querySelector('iframe').addEventListener('load', ()=>{
+      document.querySelector('#loader').style.display = 'none';
+      document.body.style.overflow = 'auto';
+    })
+  });
+} else if(type.toLowerCase() == "cv"){
+  document.querySelector('#file-name').textContent = "السيرة الذاتية";
+  document.querySelector('iframe').setAttribute('src',`../files/فهدة عابد CV.pdf`);
+  document.querySelector('#loader').style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
